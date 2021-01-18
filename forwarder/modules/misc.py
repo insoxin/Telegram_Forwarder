bd767da32bf38185d73d8c4554f851a31cefc22a
Redy <192666378@qq.com>
2021-01-18 15:04:28 +0800

    Update misc.py

--- forwarder/modules/misc.py
+++ forwarder/modules/misc.py
@@ -13,16 +13,16 @@ def get_id(update, context):
             sender = message.reply_to_message.forward_from
             forwarder = message.reply_to_message.from_user
             message.reply_text(
-                "The original sender, {}, has an ID of `{}`. \n"
-                "The forwarder, {}, has an ID of `{}`.".format(
+                "原始发件人{}的ID为`{}`. \n"
+                "转发者 {},ID为 `{}`.".format(
                     sender.first_name, sender.id,
                     forwarder.first_name, forwarder.id), parse_mode=ParseMode.MARKDOWN)
         elif message.reply_to_message.forward_from_chat:  # Replied message is a forward from a channel
             channel = message.reply_to_message.forward_from_chat
             forwarder = message.reply_to_message.from_user
             message.reply_text(
-                "The channel, {}, has an ID of `{}`. \n"
-                "The forwarder, {}, has an ID of `{}`.".format(
+                "频道,{},ID为`{}`. \n"
+                "转发者,{},ID为`{}`.".format(
                     channel.title, channel.id,
                     forwarder.first_name, forwarder.id), parse_mode=ParseMode.MARKDOWN)
         
@@ -34,10 +34,10 @@ def get_id(update, context):
         chat = update.effective_chat
         
         if chat.type == "private":  # Private chat with the bot
-            message.reply_text("Your ID is `{}`.".format(chat.id), parse_mode=ParseMode.MARKDOWN)
+            message.reply_text("你的ID是`{}`.".format(chat.id), parse_mode=ParseMode.MARKDOWN)
         
         else:  # Group chat where the bot is a member
-            message.reply_text("This group's ID is `{}`.".format(chat.id), parse_mode=ParseMode.MARKDOWN)
+            message.reply_text("这个群组ID是`{}`.".format(chat.id), parse_mode=ParseMode.MARKDOWN)
 
 
 GET_ID_HANDLER = MessageHandler(
