ca7d62ea95faa57b8c7efd943d9d9cdfab88ae49
Redy <192666378@qq.com>
2021-01-20 18:12:50 +0800

    Update __main__.py

--- forwarder/__main__.py
+++ forwarder/__main__.py
@@ -29,14 +29,37 @@ for module in ALL_MODULES:
     importlib.import_module("forwarder.modules." + module)
     
 
-def hello(update, context):
+def reply_markup1(update, context):
     bot.send_message(chat_id, '參考資料',
         reply_markup = InlineKeyboardMarkup([[
             InlineKeyboardButton('課程網站', url = 'https://github.com/mzshieh/pa19spring'),
             InlineKeyboardButton('Documentation', url = 'https://python-telegram-bot.readthedocs.io/en/stable/index.html')]]))
 
 # ...
+def nearest_stations(bot, update, count=5):
+    with open('allstations.csv', newline='', encoding='utf-8') as infile:
+        csv_reader = csv.reader(infile, delimiter=';')
+        stations = [(int(row[0]), float(row[1]), float(row[2]), row[3]) for row in csv_reader]
 
+        # distance sorting based on http://stackoverflow.com/a/28368926 by Sergey Ivanov
+        coord = (float(update.message.location.latitude), float(update.message.location.longitude))
+        pts = [geopy.Point(p[1], p[2], p[0]) for p in stations]
+        sts = [p[3] for p in stations]
+        onept = geopy.Point(coord[0], coord[1])
+        alldist = [(p, geopy.distance.distance(p, onept).m) for p in pts]
+        nearest = sorted(alldist, key=lambda x: (x[1]))[:count]
+        nearest_points = [n[0] for n in nearest]
+        nearest_distances = [n[1] for n in nearest]
+        nearest_sts = [sts[int(n.altitude)] for n in nearest_points]
+        msg = 'Nächstgelegene Stationen:'
+        for s, d, p in zip(nearest_sts, nearest_distances, nearest_points):
+            msg += '\n{} (<a href="https://www.google.de/maps?q={},{}">{:.0f}m</a>)'.format(s, p.latitude,
+                                                                                            p.longitude, d)
+
+        reply_keyboard = [[telegram.KeyboardButton(text='/Abfahrten {}'.format(n))] for n in nearest_sts]
+        bot.sendMessage(chat_id=update.message.chat_id, text=msg, parse_mode='HTML',
+                        reply_markup=telegram.ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+        
 def start(update, context):
     chat = update.effective_chat  # type: Optional[Chat]
     message = update.effective_message  # type: Optional[Message]
@@ -61,11 +84,12 @@ def help(update, context):
 def main():
     start_handler = CommandHandler("start", start, filters=Filters.user(OWNER_ID), run_async=True)
     help_handler = CommandHandler("help", help, filters=Filters.user(OWNER_ID), run_async=True)
-    hello_handler = CommandHandler("hello", hello, filters=Filters.user(OWNER_ID), run_async=True)
+    reply_markup1 = CommandHandler("reply_markup1", reply_markup1, filters=Filters.user(OWNER_ID), run_async=True)
+    nearest_stations = CommandHandler("nearest_stations", nearest_stations, filters=Filters.user(OWNER_ID), run_async=True)
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(help_handler)
-    dispatcher.add_handler(hello_handler)
-
+    dispatcher.add_handler(reply_markup1 )
+    dispatcher.add_handler(nearest_stations)
     if WEBHOOK:
         LOGGER.info("Using webhooks.")
         updater.start_webhook(listen=IP_ADDRESS,
